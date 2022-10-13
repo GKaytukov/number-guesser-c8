588013d1c3fa43092bad9b7f791a9923cdaa813d
Gerly Kaytukov <gerlyecc@yahoo.com>
2022-10-13 09:35:35 -0400

    second

---
+++ guess.js
@@ -0,0 +1,8 @@
+/*
+create a program that guess a number from 1 to 10 
+then ask us to guess that number.
+if it is higher say "too high" and ask again.
+if it is lower say "too low" and ask again.
+if it is the right number say "congratulations" and exit. 
+
+*/
